63505fecb5427ef27186cb3cd82722dec7f2c5a0
Michael Seitz <michael.s.seitz@gmail.com>
2021-09-05 10:41:03 +0200

    added some more features

--- core.js
+++ core.js
@@ -1,34 +1,28 @@
 "use strict"
-// wir geben dem Bäcker Geld, wir bekommen je nach dem wieviel Geld wir ihm gegeben haben ein anderes Teil zurück.
-// jetzt müssen wir für unser Geld arbeiten gehen, damit wir damit Brt kaufen können
 
 let hunger = 3
 let money = 0
 let time = 12
+let ownedBread = 0
+let publicBread = 5
 let bag = 0
+const log = []
 
 document.getElementById("hunger").innerHTML = hunger
 document.getElementById("money").innerHTML = money
 document.getElementById("time").innerHTML = time
-document.getElementById("goToWork").onclick = function() {kolchose()}
+document.getElementById("ownedBread").innerHTML = ownedBread
+document.getElementById("goToWork").onclick = kolchose
+document.getElementById("buyBread").onclick = buyBread
+document.getElementById("goToWorkInBakery").onclick = workInBakery
+document.getElementById("eatBread").onclick = eat
 
-function backshop(geld)
-{   
-    let teigware
-    if (geld <= 0.6)
-    {
-        teigware = "Bröttchen"
+function refreshLog() {
+    let logString = ""
+    for (let logEntry of log) {
+        logString += logEntry
     }
-    else if (geld <= 4)
-    {
-        teigware = "Brott"
-    }
-    else
-    {
-        teigware = "Piroschki"
-    }
-    console.log("Hier hast du ein " + teigware + ", Genosse!")
-    return teigware
+    document.getElementById("log").innerHTML = logString
 }
 
 function kolchose()
@@ -40,9 +34,50 @@ function kolchose()
     document.getElementById("money").innerHTML = money
     time -= timeToSpend
     document.getElementById("time").innerHTML = time
+    
+    log.unshift(`You worked ${timeToSpend} hours for the public!\n`)
+    refreshLog()
 
     if (hunger >= 10) {
-        console.log("You Starved to death");
-        document.getElementById("log").innerHTML = "You Starved to death"
+        log.unshift("You Starved to death\n")
+        refreshLog()
+    }
+    
+}
+
+function buyBread() {
+    let boughtBread = Number(document.getElementById("picesOfBread").value)
+    if (money > 0) {
+        ownedBread += boughtBread
+        money -= boughtBread*0.25
+        document.getElementById("money").innerHTML = money
+        document.getElementById("ownedBread").innerHTML = ownedBread
+        log.unshift("You bought the bread of the public!\n")
+        refreshLog()
+    }
+    else {
+        log.unshift("Go to work for the public!\n")
+        refreshLog()
+    }
+}
+
+function workInBakery() {
+    log.unshift("You worked for bread of the public!\n")
+        refreshLog()
+}
+
+function eat() {
+    if (ownedBread > 0 && hunger > 0) {
+        hunger--
+        ownedBread--
+        document.getElementById("hunger").innerHTML = hunger
+        document.getElementById("ownedBread").innerHTML = ownedBread
+
+        log.unshift("You ate the bread of the public!\n")
+        refreshLog()
+    }
+    else {
+        log.unshift("Go to Work, you hungry bum!\n")
+        refreshLog()
     }
 }
